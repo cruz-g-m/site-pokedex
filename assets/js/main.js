d3a7d749b6de3ac9404cf7a0533a47f5215efcd9
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-20 10:51:18 -0300

    Atualização de Projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,86 @@
-function convertPokemonToLi(pokemon){
-    return `
-    <li class="pokemon ${pokemon.type}">
-    <span class="number">#${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span> 
-    
-    <div class="detail">
-        <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li> `).join(' ')}
-        </ol>
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const detalheButton = document.getElementById('detalheButton')
 
-        <img src="${pokemon.photo}" 
-        alt="${pokemon.name}">
 
-    </div>
+const maxRecords = 151
+const limit = 151
+let offset = 0;
 
-</li>
-`
+
+function convertPokemonToLi(pokemon) {
+    return ` 
+            
+            <li class="pokemon ${pokemon.type}">  
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+            <div class="statsCollum">
+            <ol class="nameStats">
+            <li clss="nameStat">Hp</li>
+            <li clss="nameStat">Attack</li>
+            <li clss="nameStat">Defense</li>
+            <li clss="nameStat">Special-Attack</li>
+            <li clss="nameStat">Special-Defense</li>
+            <li clss="nameStat">Speed</li>
+            </ol>
+            <ol class="stats">
+            ${pokemon.stats.map((stat) => `<li class="stat ${stat}"> ${stat} </li>`).join('')}
+            </ol>
+            </div>
+        </li>
+        
+    `
 }
 
-const pokemonList = document.getElementById('pokemonList')
-
-    pokeApi.getPokemons().then((pokemons = []) => {
-        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
     })
+}
+
+loadPokemonItens(offset, limit)
+
+//loadMoreButton.addEventListener('click', () => {
+    //offset += limit
+    
+    //const qtdRecordNextPage = offset + limit
+
+    //if(qtdRecordNextPage >= maxRecords){
+      //  const newLimit =  maxRecords - offset
+      //  loadPokemonItens(offset, newLimit)
+        
+       // loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+   // } else {
+    //        loadPokemonItens(offset, limit)
+   // }
+
+//})
+
+
+
+function pesquisar_pokemons() {
+    let input = document.getElementById('barrapesquisa').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('pokemon');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+
+
+
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,7 +3,7 @@ const pokeApi = {}
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
     pokemon.name = pokeDetail.name
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
@@ -12,6 +12,20 @@ function convertPokeApiDetailToPokemon(pokeDetail){
     pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    
+    const stats = pokeDetail.stats.map((statSlot) => statSlot.base_stat)
+    const [stat] = stats
+
+    pokemon.stats = stats
+    pokemon.stat = stat
+
+
+
+    //const baseStats = pokeDetail.stats.base_stat
+    //const [baseStat] = baseStats
+
+   // pokemon.baseStats = baseStats
+    //pokemon.baseStat = baseStat
 
     return pokemon
 }
@@ -23,8 +37,8 @@ pokeApi.getPokemonDetail = (pokemon) => {
 }
 
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offse=${offset}&limit=${limit}`;
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
      return fetch(url)
     .then((response) => response.json()) // pedido de converção do body para json
     .then((jsonBody) => jsonBody.results) //pegando do json a lista de results
